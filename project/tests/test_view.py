38d1101ef9964e9b8714d9b826af90a10079b815
vladmspb <nevasv@yandex.ru>
2023-01-16 00:17:49 +0300

    Добавил views и templates, css , bootstrap, 404 (2)

---
+++ project/tests/test_view.py
@@ -0,0 +1,10 @@
+# from django.test import TestCase
+# from django.db import connection
+# from django.urls import reverse
+# from rest_framework.test import APITestCase
+# from flatpages.models import UserConfFlatpages
+#
+# class LogicTestStart(TestCase):
+#     pass
+#
+# class Test_Flatpages_API_view(APITestCase):
